1c86539172d262e07f032c7e1fa6f88c90bf1ab1
shr <357396211@qq.com>
2020-02-23 21:51:39 +0800

    启动功能开发暂停

--- src/main/java/com/sdt/workflow/core/service/impl/StartActivitiServiceImpl.java
+++ src/main/java/com/sdt/workflow/core/service/impl/StartActivitiServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sdt.workflow.core.service.impl;
 
 import com.sdt.workflow.core.service.StartActivitiService;
+import com.sdt.workflow.person.vo.Person;
 import org.activiti.engine.IdentityService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 工作流启动实现类
@@ -34,11 +36,15 @@ public class StartActivitiServiceImpl implements StartActivitiService {
     @Resource
     private IdentityService identityService;
 
+    @Resource
+    private HttpServletRequest request;
+
     @Override
     public void noForm(String deploymentId) {
         // 需要从session中获取人员
         ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().deploymentId(deploymentId).singleResult();
         ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
-
+        Person person = (Person) request.getSession().getAttribute("person");
+        identityService.setAuthenticatedUserId(person.getId());
     }
 }
